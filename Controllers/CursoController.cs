8cadf9f0c5f82c8d3224253848f7e8e0c4980b1e
lazaro <lazaro.11.gabriel@gmail.com>
2023-10-24 11:44:33 -0300

    Add método Put

--- Controllers/CursoController.cs
+++ Controllers/CursoController.cs
@@ -50,6 +50,16 @@ namespace apiUniversidade.Controllers
 
                 return curso;
         }
+        [HttpPut("id:int")]
+        public ActionResult Put(int id, Curso curso){
+            if(id != curso.ID)
+                return BadRequest();
+
+            _context.Entry(curso).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok(curso);
+        }
 
     }
 }
